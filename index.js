d9f1a4be02b912e06696445f7d7ddeb1708ed797
Hershy Hamerman <hershyhamerman@gmail.com>
2017-11-04 21:24:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 
-Write a function named holidayCountdown. This function should accept two
-parameters, the holiday name and number of days till that holiday. The function
-should return the string `It's ${days} days until ${holiday}!`
 function happyHolidays() {
   return "Happy Holidays!"
 }
