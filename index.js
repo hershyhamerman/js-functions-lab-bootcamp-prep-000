ef09db1646add6b80cd65f6a4c7cb0109adfd59f
Hershy Hamerman <hershyhamerman@gmail.com>
2017-11-04 21:22:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+Write a function named happyHolidays. This function should not accept any
+parameters and should return the string "Happy holidays!".
+Write a function named happyHolidaysTo. This function should accept a
+parameter of the name of the person you want to wish happy holidays, and return
+the string `Happy holidays, ${name}!`
+
+function happyHolidays() {
+  return "Happy Holidays!"
+}
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}`
+}
